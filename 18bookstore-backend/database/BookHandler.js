1c967b4ab3579cc7440359911a9d0b3565ffad23
ideans <ideans715@gmail.com>
2016-11-09 10:00:48 -0800

    Get data from DB with mongoose, BookHandler.getAll and .getOne both function and send data to api endpoint

--- 18bookstore-backend/database/BookHandler.js
+++ 18bookstore-backend/database/BookHandler.js
@@ -3,20 +3,35 @@ const Book = require('./models/BookModel')
 
 const BookHandler = {
 
-//   getAll: ( request, response, next ) => {
-//     const books = Book.findOne({ author: 'El Dood' }, function (err, docs) {
-//   // docs is an array
-// })
-//       response.status( 200 ).json({ books: books, message: 'Hoorah' })
-//   },
-
   add: ( request, response, next ) => {
     const { title, author, price } = request.body
     const book = new Book({ title: title, author: author, price: price })
     book.save()
     response.redirect('http://localhost:3000')
-  }
+  },
 
+  getAll: ( request, response, next ) => {
+    console.log("Inside get all yo")
+    Book.find( {}, ( error, data ) => { console.log( error ) })
+    .then( data => {
+      response.status( 200 ).json({ books: data, message: 'Hoorah' })
+    })
+  },
+
+  getOne: ( request, response, next ) => {
+    const { title } = request.params
+    console.log(title)
+    Book.find({ title: title }, ( error, data ) =>{ console.log( error ) })
+    .then( data => response.status(200).json({ status: 'success', book: data, message: 'Retrieved book.' }))
+  },
+
+  update: ( request, response, next ) => {
+
+  },
+
+  delete: ( request, response, next ) => {
+
+  }
 }
 
 module.exports = BookHandler
--- 18bookstore-backend/routes/index.js
+++ 18bookstore-backend/routes/index.js
@@ -6,8 +6,10 @@ router.get('/', ( request, response, next ) => {
   response.status( 200 ).json({ status: 'Yesh', message: 'More Shtuff' })
 })
 
-//router.get( '/books', BookHandler.getAll )
 router.post( '/books', BookHandler.add )
-
+router.get( '/books', BookHandler.getAll )
+router.get( '/books/:title', BookHandler.getOne )
+// router.put( '/books/:title', BookHandler.update )
+// router.delete( '/books/:title', BookHandler.delete )
 
 module.exports = router
